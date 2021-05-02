623e76bec1341a20fd2a8c487cb23b6775f65229
Craig Duckett <craig.duckett@ezyvet.com>
2021-05-03 07:47:31 +1200

    testing

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -416,14 +416,16 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
         refId: query.refId,
         fields: [{ name: 'Name', type: FieldType.string }],
       });
-
+      console.log(data)
       if (data) {
         data.forEach((object: any, index: any) => {
+          console.log(object)
           let val = { id: index, value: object };
+          console.log(val)
           frame.appendRow([val.value]);
         });
       }
-
+      console.log(frame)
       return frame;
     });
   }
