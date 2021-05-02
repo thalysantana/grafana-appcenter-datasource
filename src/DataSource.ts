a70fde0174c69212eadb6388b9528d835a72448b
Craig Duckett <craig.duckett@ezyvet.com>
2021-05-03 11:16:04 +1200

    Linting

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -389,30 +389,33 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
         ],
       });
 
-      data.reduce((
-        accumulator: any[],
-        value: {
-          name: any;
-          device_count: any;
-          previous_device_count: any;
-          count: any;
-          previous_count: any;
-          count_per_device: any;
-        }
-      ) => {
-        let name = value.name,
-          found = accumulator.find(elem => elem.name === name);
-        if (found) {
-          found.device_count += value.device_count;
-          found.previous_device_count += value.previous_device_count;
-          found.count += value.count;
-          found.previous_count += value.previous_count;
-          found.count_per_device += value.count_per_device;
-        } else {
-          accumulator.push(value);
-        }
-        return accumulator;
-      }, []);
+      data.reduce(
+        (
+          accumulator: any[],
+          value: {
+            name: any;
+            device_count: any;
+            previous_device_count: any;
+            count: any;
+            previous_count: any;
+            count_per_device: any;
+          }
+        ) => {
+          let name = value.name,
+            found = accumulator.find(elem => elem.name === name);
+          if (found) {
+            found.device_count += value.device_count;
+            found.previous_device_count += value.previous_device_count;
+            found.count += value.count;
+            found.previous_count += value.previous_count;
+            found.count_per_device += value.count_per_device;
+          } else {
+            accumulator.push(value);
+          }
+          return accumulator;
+        },
+        []
+      );
 
       data.sort(this.sortBy.bind(null, ['count desc']));
 
@@ -472,24 +475,27 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
         ],
       });
       if (data) {
-        data.reduce((
-          accumulator: any[],
-          value: {
-            name: any;
-            count: any;
-            previous_count: any;
-          }
-        ) => {
-          let name = value.name,
-            found = accumulator.find(elem => elem.name === name);
-          if (found) {
-            found.count += value.count;
-            found.previous_count += value.previous_count;
-          } else {
-            accumulator.push(value);
-          }
-          return accumulator;
-        }, []);
+        data.reduce(
+          (
+            accumulator: any[],
+            value: {
+              name: any;
+              count: any;
+              previous_count: any;
+            }
+          ) => {
+            let name = value.name,
+              found = accumulator.find(elem => elem.name === name);
+            if (found) {
+              found.count += value.count;
+              found.previous_count += value.previous_count;
+            } else {
+              accumulator.push(value);
+            }
+            return accumulator;
+          },
+          []
+        );
 
         data.sort(this.sortBy.bind(null, ['name desc']));
 
